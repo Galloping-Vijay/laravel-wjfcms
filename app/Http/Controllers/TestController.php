3cc5ae822359b112c6f4dcaabe66b9dc760efacc
vijay <1937832819@qq.com>
2019-04-03 13:38:11 +0800

    这个版本怎么了

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -12,17 +13,13 @@ class TestController extends Controller
     //
     public function index()
     {
-//        Config::set('entrust.role','App\Models\Role');
-//        Config::set('entrust.permission','App\Models\permission');
-//        Config::set('entrust.role_user_table','App\Models\RoleUser');
-        dd(Config::get('entrust'));
-        $admin= Role::where('name','=','admin')->first();
         $user = User::where('name', '=', '魏花花')->first();
-
-        //调用EntrustUserTrait提供的attachRole方法
-        $user->attachRole($admin); // 参数可以是Role对象，数组或id
-
-        // 或者也可以使用Eloquent原生的方法
-        $user->roles()->attach($admin->id); //只需传递id即可
+        echo 'owner:'. $user->hasRole('owner'); // false
+        echo "<br/>";
+        echo 'admin:'. $user->hasRole('admin'); // true
+        echo "<br/>";
+        echo 'edit-user:'.  $user->can('edit-user'); // true
+        echo "<br/>";
+        echo 'create-post:'.  $user->can('create-post'); // true
     }
 }
