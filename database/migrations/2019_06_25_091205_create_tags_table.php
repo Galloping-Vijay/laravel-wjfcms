4181fdebb8698b99c3dd2573a4e363d3ee98b830
vijay <1937832819@qq.com>
2019-06-25 17:16:15 +0800

    增加文章表数据迁移

---
+++ database/migrations/2019_06_25_091205_create_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('tags')) {
+            Schema::create('tags', function (Blueprint $table) {
+                $table->increments('id')->comment('标签表');
+                $table->string('name', 20)->default('')->comment('标签名');
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
