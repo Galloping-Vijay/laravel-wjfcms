b4368ccf97efdb0befd7c66fb36015a3536363b1
Vijay <1937832819@qq.com>
2019-08-11 23:14:47 +0800

    '编辑器修改'

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -46,7 +46,8 @@
                 <div class="layui-upload">
                     <div class="layui-upload-list">
                         <input type="hidden" name="cover" value="">
-                        <img class="layui-upload-img" id="up_cover" src="/images/config/default-img.jpg" style="cursor:pointer">
+                        <img class="layui-upload-img" id="up_cover" src="/images/config/default-img.jpg"
+                             style="cursor:pointer">
                         <p id="up_cover_text"></p>
                     </div>
                 </div>
@@ -111,6 +112,108 @@
                 , upload = layui.upload
                 , form = layui.form;
             var csrf_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+            layedit.set({
+                //暴露layupload参数设置接口 --详细查看layupload参数说明
+                uploadImage: {
+                    url: '/admin/article/uploadImage' //接口url
+                    , type: 'post'//默认post
+                    , headers: {
+                        'X-CSRF-TOKEN': csrf_token
+                    }
+                    , data: {
+                        _token: csrf_token
+                    }
+                    , done: function (data) {
+                        console.log(data);
+                    }
+                }
+                , uploadVideo: {
+                    url: '/admin/article/uploadImage', //接口url
+                    headers: {
+                        'X-CSRF-TOKEN': csrf_token
+                    },
+                    data: {
+                        _token: csrf_token
+                    },
+                    accept: 'video',
+                    acceptMime: 'video/*',
+                    exts: 'mp4|flv|avi|rm|rmvb',
+                    size: 1024 * 10 * 2,
+                    done: function (data) {
+                        console.log(data);
+                    }
+                }
+                , uploadFiles: {
+                    url: '/admin/article/uploadImage' ,//接口url
+                    headers: {
+                        'X-CSRF-TOKEN': csrf_token
+                    },
+                    data: {
+                        _token: csrf_token
+                    },
+                    accept: 'file',
+                    acceptMime: 'file/*',
+                    size: '20480',
+                    done: function (data) {
+                        console.log(data);
+                    }
+                }
+                //右键删除图片/视频时的回调参数，post到后台删除服务器文件等操作，
+                //传递参数：
+                //图片： imgpath --图片路径
+                //视频： filepath --视频路径 imgpath --封面路径
+                , calldel: {
+                    url: '/admin/article/uploadImage', //接口url
+                    headers: {
+                        'X-CSRF-TOKEN': csrf_token
+                    },
+                    data: {
+                        _token: csrf_token
+                    },
+                    done: function (data) {
+                        console.log(data);
+                    }
+                }
+                //开发者模式 --默认为false
+                , devmode: true
+                //插入代码设置 --hide:true 等同于不配置codeConfig
+                , codeConfig: {
+                    hide: false,  //是否显示编码语言选择框
+                    default: 'javascript' //hide为true时的默认语言格式
+                }
+                //新增iframe外置样式和js
+                //, quote:{
+                //    style: ['/Content/Layui-KnifeZ/css/layui.css','/others'],
+                //    js: ['/Content/Layui-KnifeZ/lay/modules/jquery.js']
+                //}
+                //自定义样式-暂只支持video添加
+                //, customTheme: {
+                //    video: {
+                //        title: ['原版', 'custom_1', 'custom_2']
+                //        , content: ['', 'theme1', 'theme2']
+                //        , preview: ['', '/images/prive.jpg', '/images/prive2.jpg']
+                //    }
+                //}
+                //插入自定义链接
+                , customlink: {
+                    title: '插入layui官网'
+                    , href: 'https://www.layui.com'
+                    , onmouseup: ''
+                }
+                , facePath: 'http://knifez.gitee.io/kz.layedit/Content/Layui-KnifeZ/'
+                , videoAttr: ' preload="none" '
+                , tool: [
+                    'html', 'undo', 'redo', 'code', 'strong', 'italic', 'underline', 'del', 'addhr', '|', 'fontFomatt', 'fontfamily', 'fontSize', 'fontBackColor', 'colorpicker', 'face'
+                    , '|', 'left', 'center', 'right', '|', 'link', 'unlink', 'images', 'image_alt', 'video', 'attachment', 'anchors'
+                    , '|'
+                    , 'table', 'customlink'
+                    , 'fullScreen'
+                ]
+                , height: '600px'
+            });
+            var layedit_index = layedit.build('content');
+
             //图片上传
             var uploadInst = upload.render({
                 elem: '#up_cover'
@@ -145,18 +248,6 @@
                     });
                 }
             });
-
-            layedit.set({
-                uploadImage: {
-                    url: '/admin/article/uploadImage' //接口url
-                    , type: 'post'//默认post
-                    , data: {
-                        _token: csrf_token
-                    }
-                }
-            });
-            //注意：layedit.set 一定要放在 build 前面，否则配置全局接口将无效。
-            var layedit_index = layedit.build('content'); //建立编辑器
 
             //监听指定开关
             form.on('switch(status)', function () {
--- resources/views/admin/article/edit.blade.php
+++ resources/views/admin/article/edit.blade.php
@@ -156,13 +156,103 @@
             });
 
             layedit.set({
+                //暴露layupload参数设置接口 --详细查看layupload参数说明
                 uploadImage: {
                     url: '/admin/article/uploadImage' //接口url
                     , type: 'post'//默认post
+                    , headers: {
+                        'X-CSRF-TOKEN': csrf_token
+                    }
                     , data: {
                         _token: csrf_token
                     }
+                    , done: function (data) {
+                        console.log(data);
+                    }
                 }
+                , uploadVideo: {
+                    url: '/admin/article/uploadImage', //接口url
+                    headers: {
+                        'X-CSRF-TOKEN': csrf_token
+                    },
+                    data: {
+                        _token: csrf_token
+                    },
+                    accept: 'video',
+                    acceptMime: 'video/*',
+                    exts: 'mp4|flv|avi|rm|rmvb',
+                    size: 1024 * 10 * 2,
+                    done: function (data) {
+                        console.log(data);
+                    }
+                }
+                , uploadFiles: {
+                    url: '/admin/article/uploadImage' ,//接口url
+                    headers: {
+                        'X-CSRF-TOKEN': csrf_token
+                    },
+                    data: {
+                        _token: csrf_token
+                    },
+                    accept: 'file',
+                    acceptMime: 'file/*',
+                    size: '20480',
+                    done: function (data) {
+                        console.log(data);
+                    }
+                }
+                //右键删除图片/视频时的回调参数，post到后台删除服务器文件等操作，
+                //传递参数：
+                //图片： imgpath --图片路径
+                //视频： filepath --视频路径 imgpath --封面路径
+                , calldel: {
+                    url: '/admin/article/uploadImage', //接口url
+                    headers: {
+                        'X-CSRF-TOKEN': csrf_token
+                    },
+                    data: {
+                        _token: csrf_token
+                    },
+                    done: function (data) {
+                        console.log(data);
+                    }
+                }
+                //开发者模式 --默认为false
+                , devmode: true
+                //插入代码设置 --hide:true 等同于不配置codeConfig
+                , codeConfig: {
+                    hide: false,  //是否显示编码语言选择框
+                    default: 'javascript' //hide为true时的默认语言格式
+                }
+                //新增iframe外置样式和js
+                //, quote:{
+                //    style: ['/Content/Layui-KnifeZ/css/layui.css','/others'],
+                //    js: ['/Content/Layui-KnifeZ/lay/modules/jquery.js']
+                //}
+                //自定义样式-暂只支持video添加
+                //, customTheme: {
+                //    video: {
+                //        title: ['原版', 'custom_1', 'custom_2']
+                //        , content: ['', 'theme1', 'theme2']
+                //        , preview: ['', '/images/prive.jpg', '/images/prive2.jpg']
+                //    }
+                //}
+                //插入自定义链接
+                , customlink: {
+                    title: '插入layui官网'
+                    , href: 'https://www.layui.com'
+                    , onmouseup: ''
+                }
+                , facePath: 'http://knifez.gitee.io/kz.layedit/Content/Layui-KnifeZ/'
+                , videoAttr: ' preload="none" '
+                , tool: [
+                    'html', 'undo', 'redo', 'code', 'strong', 'italic', 'underline', 'del', 'addhr', '|', 'fontFomatt', 'fontfamily', 'fontSize', 'fontBackColor', 'colorpicker', 'face'
+                    , '|', 'left', 'center', 'right', '|', 'link', 'unlink', 'images', 'image_alt', 'video', 'attachment', 'anchors'
+                    , '|'
+                    , 'table', 'customlink'
+                    , 'fullScreen'
+                ]
+                , height: '600px'
             });
             //注意：layedit.set 一定要放在 build 前面，否则配置全局接口将无效。
             var layedit_index = layedit.build('content'); //建立编辑器
