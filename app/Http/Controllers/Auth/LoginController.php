f53badb8747bd19385ff7b99564df359ef1caf7b
Vijay <1937832819@qq.com>
2019-09-21 20:33:30 +0800

    '验证码'

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Nav;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\View;
 
 class LoginController extends Controller
 {
@@ -35,5 +38,21 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        $navList = Nav::getMenuTree(Nav::orderBy('sort', 'asc')->get()->toArray());
+        $categories = Category::getMenuTree(Category::orderBy('sort', 'asc')->select('id', 'name', 'pid')->get()->toArray());
+        View::share([
+            'nav_list' => $navList,
+            'category_list' => $categories,
+        ]);
+        return view('auth.login');
     }
 }
