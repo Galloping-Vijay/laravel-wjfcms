7238bcb88da839a96dde0f5da58cda759f78d0ce
Vijay <1937832819@qq.com>
2019-11-07 20:24:36 +0800

    '添加自动提交'

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -254,6 +254,21 @@
             $(this).addClass('current').siblings().removeClass('current');
         });
     </script>
+    <script>
+        //自动推送功能
+        (function(){
+            var bp = document.createElement('script');
+            var curProtocol = window.location.protocol.split(':')[0];
+            if (curProtocol === 'https') {
+                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            }
+            else {
+                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+            }
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(bp, s);
+        })();
+    </script>
 @show
 </body>
 </html>
