ede2757d99d7e3969085ebd0b3b932c8060cc872
Vijay <1937832819@qq.com>
2019-11-12 20:24:11 +0800

    '主动推送'

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -34,6 +34,21 @@
             s.parentNode.insertBefore(hm, s);
         })();
     </script>
+    <script>
+        //自动推送功能
+        (function(){
+            var bp = document.createElement('script');
+            var curProtocol = window.location.protocol.split(':')[0];
+            if (curProtocol === 'https') {
+                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            }
+            else {
+                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+            }
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(bp, s);
+        })();
+    </script>
     @yield('header')
     <script>
         window.onload = function () {
@@ -254,21 +269,6 @@
         $('#toolbar-menu li').click(function () {
             $(this).addClass('current').siblings().removeClass('current');
         });
-    </script>
-    <script>
-        //自动推送功能
-        (function(){
-            var bp = document.createElement('script');
-            var curProtocol = window.location.protocol.split(':')[0];
-            if (curProtocol === 'https') {
-                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-            }
-            else {
-                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-            }
-            var s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(bp, s);
-        })();
     </script>
 @show
 </body>
