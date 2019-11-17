727e497a7d3b322e4d46dfe6224cbd56be4237a1
Vijay <1937832819@qq.com>
2019-11-17 12:15:51 +0800

    'ss'

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\LogInfo;
 use App\Console\Commands\SendLinkSubmit;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        LogInfo::class,
     ];
 
     /**
@@ -28,6 +30,8 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command(SendLinkSubmit::class,['--force'])->everyTenMinutes()->runInBackground();
+        //log
+        $schedule->command(LogInfo::class,['--force'])->everyTenMinutes()->runInBackground();
     }
 
     /**
