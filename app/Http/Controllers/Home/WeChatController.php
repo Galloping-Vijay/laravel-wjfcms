9055b685a5d70f0397d5f1456562c04043693fc1
Vijay <1937832819@qq.com>
2019-11-24 20:18:05 +0800

    '配置微信'

--- app/Http/Controllers/Home/WeChatController.php
+++ app/Http/Controllers/Home/WeChatController.php
@@ -25,6 +25,12 @@ class WeChatController extends Controller
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
         $app = app('wechat.official_account');
+
+        $app->server->push(function($message){
+            return $message['MsgType'];
+        });
+
+        return $app->server->serve();
         $app->server->push(function ($message) use (&$app) {
             //return "欢迎关注 心若野马";
             switch ($message['MsgType']) {
