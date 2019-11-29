8466dfb9cffa233dd533db20d5e4df9d715aa824
Vijay <1937832819@qq.com>
2019-11-29 18:38:49 +0800

    'auth登录'

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthenticationController extends Controller
+{
+    /**
+     * Description:未授权跳转页
+     * User: Vijay <1937832819@qq.com>
+     * Date: 2019/11/29
+     * Time: 18:36
+     * @param $account
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function getSocialRedirect($account)
+    {
+        try {
+            return Socialite::with($account)->redirect();
+        } catch (\InvalidArgumentException $e) {
+            return redirect('/login');
+        }
+    }
+
+    /**
+     * Description:
+     * User: Vijay <1937832819@qq.com>
+     * Date: 2019/11/29
+     * Time: 18:36
+     * @param $account
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function getSocialCallback($account)
+    {
+        // 从第三方 OAuth 回调中获取用户信息
+        $socialUser = Socialite::with($account)->user();
+        // 在本地 users 表中查询该用户来判断是否已存在
+        $user = User::where('provider_id', '=', $socialUser->id)
+            ->where('provider', '=', $account)
+            ->first();
+        if ($user == null) {
+            // 如果该用户不存在则将其保存到 users 表
+            $newUser = new User();
+            $newUser->name = $socialUser->getName();
+            $newUser->email = $socialUser->getEmail() == '' ? '' : $socialUser->getEmail();
+            $newUser->avatar = $socialUser->getAvatar();
+            $newUser->password = '';
+            $newUser->provider = $account;
+            $newUser->provider_id = $socialUser->getId();
+            $newUser->save();
+            $user = $newUser;
+        }
+        // 手动登录该用户
+        Auth::login($user);
+        // 登录成功后将用户重定向到首页
+        return redirect('/');
+    }
+}
