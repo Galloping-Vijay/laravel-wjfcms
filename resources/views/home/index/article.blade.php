10272dee91654fefd66b61dc93e4485109b0cc99
Vijay <1937832819@qq.com>
2019-12-02 11:09:13 +0800

    's'

--- resources/views/home/index/article.blade.php
+++ resources/views/home/index/article.blade.php
@@ -95,6 +95,15 @@
                     });
                 })
             </script>
+            <!--评论PC版-->
+            <div id="SOHUCS" sid="{{ $info->id }}"></div>
+            <script charset="utf-8" type="text/javascript" src="https://changyan.sohu.com/upload/changyan.js" ></script>
+            <script type="text/javascript">
+                window.changyan.api.config({
+                    appid: 'cyuz620gF',
+                    conf: 'prod_3ef1231e159e9e6b689e2bd15babaa0c'
+                });
+            </script>
             <div class="nextinfo">
                 <p>上一篇：
                     @if(!empty($pre))
