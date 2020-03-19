85d7183b6804154cc7aa31dad42777bd35a5621c
Vijay <1937832819@qq.com>
2020-03-19 19:31:22 +0800

    '后台用户密码加密'

--- app/Http/Controllers/Home/ToolsController.php
+++ app/Http/Controllers/Home/ToolsController.php
@@ -20,7 +20,7 @@ class ToolsController extends Controller
      */
     public function linkSubmit()
     {
-        $urls = array(
+        $urls    = array(
             'https://www.choudalao.com/',
             'https://www.choudalao.com/chat',
             'https://www.choudalao.com/category/1',
@@ -39,23 +39,23 @@ class ToolsController extends Controller
             'https://www.choudalao.com/tag/7',
             'https://www.choudalao.com/tag/8',
         );
-        $api = 'http://data.zz.baidu.com/urls?site=https://www.choudalao.com&token=nsdmyfRcySMSxYl1';
-        $ch = curl_init();
+        $api     = 'http://data.zz.baidu.com/urls?site=https://www.choudalao.com&token=nsdmyfRcySMSxYl1';
+        $ch      = curl_init();
         $options = array(
-            CURLOPT_URL => $api,
-            CURLOPT_POST => true,
+            CURLOPT_URL            => $api,
+            CURLOPT_POST           => true,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POSTFIELDS => implode("\n", $urls),
-            CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
+            CURLOPT_POSTFIELDS     => implode("\n", $urls),
+            CURLOPT_HTTPHEADER     => array('Content-Type: text/plain'),
         );
         curl_setopt_array($ch, $options);
         $result = curl_exec($ch);
-        $res = json_decode($result, true);
+        $res    = json_decode($result, true);
         Cache::put('link_remain', $res['remain']);
         $remainArr = [
             'link_remain' => Cache::get('link_remain')
         ];
-        $res = array_merge($res, $remainArr);
+        $res       = array_merge($res, $remainArr);
         return response()->json($res);
     }
 
@@ -84,12 +84,12 @@ class ToolsController extends Controller
     public function getEmailCode(Request $request)
     {
         $email = $request->input('email');
-        $info = User::where('email', $email)->first();
+        $info  = User::where('email', $email)->first();
         if ($info) {
             $data = [
-                'code' => 1,
-                'msg' => '该邮箱已被注册',
-                'data' => [],
+                'code'        => 1,
+                'msg'         => '该邮箱已被注册',
+                'data'        => [],
                 'create_time' => date('Y-m-d H:i:s', time())
             ];
             return response()->json($data);
@@ -98,24 +98,34 @@ class ToolsController extends Controller
         $code = mt_rand(1000, 9999);
         Cache::put($email, $code, 60);
         $subject = env('MAIL_FROM_NAME');
-        $msg = '【臭大佬】邮箱验证码：' . $code . '，如非本人操作，请忽略本消息。';
-        Mail::raw($msg, function ($message) use ($email, $subject) {
-            $message->to($email)->subject($subject);
-        });
-        //错误处理
-        if (count(Mail::failures()) < 1) {
+        $msg     = '【臭大佬】邮箱验证码：' . $code . '，如非本人操作，请忽略本消息。';
+        try {
+            Mail::raw($msg, function ($message) use ($email, $subject) {
+                $message->to($email)->subject($subject);
+            });
+            //错误处理
+            if (count(Mail::failures()) < 1) {
+                $data = [
+                    'code'        => 0,
+                    'msg'         => '发送邮件成功，请查收！',
+                    'data'        => [],
+                    'create_time' => date('Y-m-d H:i:s', time())
+                ];
+                return response()->json($data);
+            } else {
+                $data = [
+                    'code'        => 1,
+                    'msg'         => '邮箱验证码发送失败,请联系臭大佬(微信:wjf1937832819)',
+                    'data'        => [],
+                    'create_time' => date('Y-m-d H:i:s', time())
+                ];
+                return response()->json($data);
+            }
+        } catch (\Exception $e) {
             $data = [
-                'code' => 0,
-                'msg' => '发送邮件成功，请查收！',
-                'data' => [],
-                'create_time' => date('Y-m-d H:i:s', time())
-            ];
-            return response()->json($data);
-        } else {
-            $data = [
-                'code' => 1,
-                'msg' => '邮箱验证码发送失败,请联系臭大佬(微信:wjf1937832819)',
-                'data' => [],
+                'code'        => 1,
+                'msg'         => '邮箱配置错误，请联系管理员',
+                'data'        => [],
                 'create_time' => date('Y-m-d H:i:s', time())
             ];
             return response()->json($data);
