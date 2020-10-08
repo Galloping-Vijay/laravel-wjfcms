7a8adff42cd2a0e7f9ca2d251f5d0ddc6ed01cb6
lvyanlin <waiwad@163.com>
2020-10-08 13:49:47 +0800

    '去掉日志等'

--- resources/views/layouts/home/ad.blade.php
+++ resources/views/layouts/home/ad.blade.php
@@ -1,7 +1,16 @@
 <div class="cloud" style="margin-top: 20px;">
     <h2 class="hometitle">广告栏</h2>
     <ul>
-
+        <div class="_jqxom5ik70q" style="margin: -20px;"></div>
+        <script type="text/javascript">
+            (window.slotbydup = window.slotbydup || []).push({
+                id: "u6209999",
+                container: "_jqxom5ik70q",
+                async: true
+            });
+        </script>
+        <!-- 多条广告如下脚本只需引入一次 -->
+        <script type="text/javascript" src="//cpro.baidustatic.com/cpro/ui/cm.js" async="async" defer="defer" >
+        </script>
     </ul>
 </div>
-<script data-ad-client="ca-pub-4179312594817779" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
